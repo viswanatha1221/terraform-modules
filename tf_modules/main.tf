c195bc5e2348d97b0e09d44cf1f277dcf5f51e90
Viswanatha Reddy Kandanuru <viswanatha.kanda@gmail.com>
2025-05-20 10:18:07 -0500

    Update main.tf

--- tf_modules/main.tf
+++ tf_modules/main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
-  source      = "./modules/vpc"
+  source      = "./tf_modules/vpc"
   vpc_cidr    = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
 
 module "sg" {
-  source = "./modules/sg"
+  source = "./tf_modules/sg"
   vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
-  source  = "./modules/ec2"
+  source  = "./tf_modules/ec2"
   sg_id   = module.sg.sg_id
   subnets = module.vpc.subnet_ids
 }
