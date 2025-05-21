3b2fafebceedf372140d405185239e3b8492e51b
Dinesh9064 <kokkanti.dinesh@gmail.com>
2025-05-21 16:02:28 -0500

    added bastion host

--- tf_modules/sg/outputs.tf
+++ tf_modules/sg/outputs.tf
@@ -1,7 +1,3 @@
-output "sg_id"{
-    value = aws_security_group.sg.id
-}
-
 output "bastion_sg_id" {
   value = aws_security_group.bastion_sg.id
 }
--- tf_modules/vpc/outputs.tf
+++ tf_modules/vpc/outputs.tf
@@ -2,13 +2,10 @@ output "vpc_id" {
     value = aws_vpc.my_vpc.id
 }
 
-output "subnet_ids" {
-    value = aws_subnet.subnets.*.id
-}
-
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
+
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
