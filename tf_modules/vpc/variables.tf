97eac9ad1dd61d3fde4fe4595dbdc638541114ce
Dinesh9064 <kokkanti.dinesh@gmail.com>
2025-05-21 16:20:29 -0500

    added bastion host

--- tf_modules/vpc/variables.tf
+++ tf_modules/vpc/variables.tf
@@ -3,13 +3,16 @@ variable "vpc_cidr" {
   type = string
 }
 
-variable "subnet_cidr" {
-    description = "Subnet CIDRS"
-    type = list(string)
-}
-
 variable "subnet_names" {
     description = "Subnet names"
     type = list(string)
     default = [ "PublicSubnet1", "PublicSubnet2" ]
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
 }
