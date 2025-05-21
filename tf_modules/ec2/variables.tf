1aa850ab41a053475fcbc976cffb7fd47d888d82
Dinesh9064 <kokkanti.dinesh@gmail.com>
2025-05-21 16:00:46 -0500

    added bastion host

--- tf_modules/ec2/variables.tf
+++ tf_modules/ec2/variables.tf
@@ -1,13 +1,3 @@
-variable "sg_id" {
-  description = "SG ID for EC2"
-  type = string
-}
-
-variable "subnets" {
-  description = "Subnets for EC2"
-  type = list(string)
-}
-
 variable "ec2_names" {
     description = "EC2 names"
     type = list(string)
